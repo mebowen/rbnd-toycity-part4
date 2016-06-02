cbcfe030a598ef9125353b9150161b167f98d656
marlenaonrails <marlenabowen@gmail.com>
2016-06-02 13:29:08 +0000

    having difficulty, keep getting NoMethodError for brand from line 122 in udacidata_tests.rb

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -1,20 +1,15 @@
 class Module
   def create_finder_methods(*attributes)
-    # Your code goes here!
-    # Hint: Remember attr_reader and class_eval
-    attributes.each do |field|
-      new_method = %Q{
-        def self.find_by_#{field}(argument)
-          all = self.all()
-          found = all.select{|item| item.#{field} == argument}
-          if found.length > 0
-            return found[0]
-          else
-            return nil
-          end
-        end
-      }
-      class_eval(new_method)
+    attributes.each do |attribute|
+      define_find_by(attribute)
+    end 
+  end
+      
+  def define_find_by(attribute)
+    define_method("find_by_#{attribute}") do |argument|
+      all.each do |item|
+         return item if item.send(attribute) == argument
+      end
     end
   end
 end
