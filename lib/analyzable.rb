70bded2be86e1d66e9f50e125c8a2ca1dd9f8e97
marlenaonrails <marlenabowen@gmail.com>
2016-06-06 19:43:10 +0000

    passing all tests in analyzable_test.rb

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,25 +1,39 @@
 module Analyzable
-  
+
   def average_price(products)
-    #all_products.each do |price|
-    #sum_of_price = price += price
-    #sum_of_prices / all_products.count
+    total_of_prices = 0 
+    products.each do |product|
+      total_of_prices += product.price.to_f
+    end
+    average_price_variable = (total_of_prices / products.length).round(2)
+    return average_price_variable
+  end
+
+  
+  def print_report(argument)
+      report = "Average price of products: $#{average_price(argument)} \n"
+      count_by_brand(argument).each do |brand, count| 
+        report << "Number of products for #{brand} brand: #{count} \n" 
+      end
+      count_by_name(argument).each do |name, count|
+        report << "Number of products named #{name}: #{count} \n" 
+      end
+      return report 
   end
   
-  def print_report
-      #self.print.to_s
-      #CSV formatting 
+  def count_by_brand(products)
+    brands = products.map {|p| p.brand} 
+    count_by_attribute(brands)
   end
   
-  def count_by_brand
-      #hash
-      #all_products.each do |brand|
-      #brands_hash = {brand, count}
+  def count_by_name(products)
+    names = products.map {|p| p.name}
+    count_by_attribute(names)
   end
-  
-  def count_by_name
-      #hash
-      #all_products.each do |name|
-      #names_hash = {name, count} 
-  end
+end
+
+
+def count_by_attribute(attribute)
+  attribute_hash = attribute.inject(Hash.new(0)) {|result, element| result[element] += 1 ;result}
+  attribute_hash
 end
